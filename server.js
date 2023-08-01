98d71e27221683fb0df4595f24c0d4b5d932f8fb
Claudia Lam <claudiaslam@gmail.com>
2023-08-01 14:41:20 -0700

    import fs and path modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('node:http');
-const { PORT } = require("./config");
+const fs = require('node:fs');
+const path = require('node:path');
 
+const { PORT } = require("./config");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
