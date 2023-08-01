d6ca01b48303a8e064b56e8d8e78443890fef978
Claudia Lam <claudiaslam@gmail.com>
2023-08-01 13:45:43 -0700

    add request handling

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello World");
+  res.end();
+});
+
+//run command node server.js will print this out in terminal
+server.listen(8000, () => console.log("Server running on port 8000!"));
+
