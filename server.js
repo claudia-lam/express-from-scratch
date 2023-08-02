bb01499689f6594721dd99abc3b8a1946a9c02bb
Claudia Lam <claudiaslam@gmail.com>
2023-08-01 17:15:01 -0700

    add response to serve an html file

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const path = require('node:path');
 
 const { PORT } = require("./config");
 
+//set up http server using node.js
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./public/index.html", (err, data) => {
+    res.setHeader('Content-Type', 'text/html');
+    if (err) {
+      res.writeHead(500);
+      return res.end('An error occured');
+    }
+    //sends an http status code
+    res.writeHead(200);
+    res.end(data);
+  });
+
+  //when request is made, server responds with "hello world"
   res.write("Hello World");
-  res.end();
+  //response headers and body are sent. Server closes connection
+
 });
 
 //run command node server.js will print this out in terminal
